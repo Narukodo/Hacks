c02848ca53d12841bf6870ee82f34342c776b48b
Narukodo <mrnarukodo@gmail.com>
2017-03-11 18:16:28 -0500

    Scripts
    added test scripts

---
+++ Assets/_Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	// how much would a wood chucker chuck if a wood chucker could chuck wood?
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
